863edf96de39a6a7a7ba71ca460a2c93f89c7d91
Daniel Woodall <dtwoodall@gmail.com>
2017-10-13 13:37:29 -0700

    Testing possible path issues on GitHub Pages

--- src/components/App.js
+++ src/components/App.js
@@ -33,7 +33,7 @@ class App extends Component {
 
       <div>
 
-        <Route exact path="/" component={TaskList} />
+        <Route component={TaskList} />
 
         <Route exact path="/tasks" component={TaskList} />
         <Route exact path="/tasks/:taskId" component={TaskView} />
