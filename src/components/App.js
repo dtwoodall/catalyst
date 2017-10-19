91e993b568beff79e96b2782ae31ade4a817222b
Daniel Woodall <dtwoodall@gmail.com>
2017-10-19 13:35:06 -0700

    Testing environments

--- src/components/App.js
+++ src/components/App.js
@@ -41,6 +41,8 @@ class App extends Component {
         <Route exact path="/categories" component={CategoryList} />
         <Route exact path="/categories/:categoryId" component={CategoryView} />
 
+        { process.env.NODE_ENV }
+
         <Drawer
           open={isMenuOpen}
           onRequestClose={closeMenu}
